558ac998bcd2eb3612cedb7eef926b589e7e68f2
wenig <info@pwenig.de>
2015-06-13 18:41:33 +0200

    pg

--- endpoints/generate_api_key.js
+++ endpoints/generate_api_key.js
@@ -2,7 +2,6 @@ var uuid = require('node-uuid'),
     db = require('./../database')
 
 var action = function() {
-  console.log("test")
   var apiKeys = generateApiKeys()
   return 'secret: ' + apiKeys[0] + '<br>api-key: ' + apiKeys[1]
 }
@@ -14,8 +13,6 @@ module.exports = action()
 function generateApiKeys(){
   var secret = uuid.v4()
   var api_key = uuid.v4()
-
-  //db.callQuery("INSERT INTO users (secret, api_key) VALUES ('" + secret + "', '" + api_key + "');")
-
+  db.callQuery("INSERT INTO users (secret, api_key) VALUES ('" + secret + "', '" + api_key + "');")
   return [uuid.v4(), uuid.v4()]
 }

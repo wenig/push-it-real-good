6cfd0d106d079d90fdb9fa6d349893192bfd8320
wenig <info@pwenig.de>
2015-06-13 20:22:38 +0200

    ws

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function(req, res) {
   res.send('hello world')
 })
 
-app.post('/', urlencodedParser, function(req, res){
+app.post('/', jsonParser, function(req, res){
   console.log(req.body)
   res.send(PushItRealGood.go(req.body.auth_tokens, req.body.secret, req.body.api_key, req.body.message))
 })
